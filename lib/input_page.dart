96159beb15426916f849f0be971f64fa0023cab8
Ricardo Serrano <rxsh964@gmail.com>
2020-02-20 03:55:12 -0500

    refactoring icon content

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/icon_content.dart';
 import 'package:bmi_calculator/my_card.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -21,48 +22,18 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 Expanded(
                   child: MyCard(
-                    cardChild: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          FontAwesomeIcons.mars,
-                          size: 80.0,
-                        ),
-                        SizedBox(
-                          height: 15.0,
-                        ),
-                        Text(
-                          'MALE',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            color: Color(0xFF8D8E98),
-                          ),
-                        )
-                      ],
+                    cardChild: IconContent(
+                      genderIcon: FontAwesomeIcons.mars,
+                      genderText: 'MALE',
                     ),
                     color: Color(0xFF1D1E33),
                   ),
                 ),
                 Expanded(
                   child: MyCard(
-                    cardChild: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          FontAwesomeIcons.venus,
-                          size: 80.0,
-                        ),
-                        SizedBox(
-                          height: 15.0,
-                        ),
-                        Text(
-                          'FEMALE',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            color: Color(0xFF8D8E98),
-                          ),
-                        )
-                      ],
+                    cardChild: IconContent(
+                      genderIcon: FontAwesomeIcons.venus,
+                      genderText: 'FEMALE',
                     ),
                     color: Color(0xFF1D1E33),
                   ),
